56836d8a0f6feb80f6860c5d108294d26c682f48
Ke Wang <wangkekekexili@gmail.com>
2014-11-03 11:38:12 -0500

    First Commit
    ColorHelper class

--- test/Test.java
+++ test/Test.java
@@ -2,11 +2,17 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javafx.util.Pair;
 
 import javax.imageio.ImageIO;
+
+import colorworld.crawler.Crawler;
+import colorworld.crawler.Download;
+import colorworld.image.ColorHelper;
 
 public class Test {
 
@@ -27,29 +33,16 @@ public class Test {
 		return Math.sqrt(distance);
 	}
 	
-	public static void main(String[] args) 
-	throws IOException{
-		int count[][][] = new int[4][4][4]; 
-		int count1[][][] = new int[4][4][4];
-		File tempImageFile = new File("data/14.jpg");
-		File tempImageFile2 = new File("data/25.jpg");
-		
-		BufferedImage image = ImageIO.read(tempImageFile);
-		BufferedImage image2 = ImageIO.read(tempImageFile2);
-		int width = image.getWidth();
-		int height = image.getHeight();
-		int count11 = 0;
-		for (int i = 0;i != height;i++) {
-			for (int j = 0;j != width;j++) {
-				int c = image.getRGB(j, i);
-				int c1 = image2.getRGB(j,i);
-			
-				if (c!=c1) {
-					count11++;
-				}
-			}
-		}
-		System.out.println(count11);
+	public static void main(String[] args) throws MalformedURLException, IOException {
+
+		//Download.saveFile(new URL("http://www.cs.washington.edu/research/imagedatabase/groundtruth/_tars.for.download/arborgreens.tar"), "downloads/a.tar");
+		//URL u = new URL("http://www.cs.washington.edu/research/imagedatabase/groundtruth/_tars.for.download/arborgreens.tar");
+		//System.out.println(u.getFile());
+		//Crawler.downloadAllTarFiles("http://www.cs.washington.edu/research/imagedatabase/groundtruth/_tars.for.download/");
+		int rgb[] = {1,2,5};
+		double hsv[] = ColorHelper.RGB2HSV(rgb);
+		for (int i = 0;i !=3;i++)
+			System.out.println(hsv[i]);
 	}
 
 }
