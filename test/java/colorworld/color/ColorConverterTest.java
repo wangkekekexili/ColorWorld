ca675d8f11b3e37b30bd1bcb402aa28fb081bea2
Ke Wang <wangkekekexili@gmail.com>
2015-01-31 16:49:24 -0500

    add tests

---
+++ test/java/colorworld/color/ColorConverterTest.java
@@ -0,0 +1,12 @@
+package colorworld.color;
+
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class ColorConverterTest {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ test/java/colorworld/image/ImageConverterTest.java
@@ -0,0 +1,17 @@
+package colorworld.image;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageConverterTest {
+
+	public static void main(String[] args) throws IOException {
+		BufferedImage input = ImageIO.read(new File("test/data/lake.jpg"));
+		BufferedImage output = ImageConverter.flipVertical(input);
+		ImageIO.write(output, "jpg", new File("test/data/result.jpg"));
+	}
+
+}
