a1500dc0fe3dbe7ed9b68574a96a2f9c5bd87c3c
Ke Wang <wangkekekexili@gmail.com>
2015-02-03 01:34:02 -0500

    add RGB to XYZ method

--- main/java/colorworld/color/ColorConverter.java
+++ main/java/colorworld/color/ColorConverter.java
@@ -1,8 +1,8 @@
 package colorworld.color;
 
 /**
- * ColorConverter converts colors in one color space
- * to another
+ * ColorConverter converts colors from RGB color space to
+ * another or from the other to RGB.
  * 
  * @author Ke Wang
  *
@@ -93,4 +93,38 @@ public class ColorConverter {
 				((int)array[2] & 0xFF);
 	}
 	
+	/**
+	 * Convert from RGB color space to XYZ color space
+	 * 
+	 * @param rgb a double array of size 3 containing RGB data
+	 * @return a double array of size 3 containing XYZ
+	 */
+	public static double[] rgbToXyz(double[] rgb) {
+		double r = rgb[0]/255;
+		double g = rgb[0]/255;
+		double b = rgb[0]/255;
+		if (r > 0.04045) {
+			r = Math.pow((r+0.055)/1.055, 2.4);
+		} else {
+			r = r/ 12.92;
+		}
+		if (g > 0.04045) {
+			g = Math.pow((g+0.055)/1.055, 2.4);
+		} else {
+			g = g/ 12.92;
+		}
+		if (b > 0.04045) {
+			b = Math.pow((b+0.055)/1.055, 2.4);
+		} else {
+			b = b/ 12.92;
+		}
+		r *= 100;
+		g *= 100;
+		b *= 100;
+		double[] xyz = new double[3];
+		xyz[0] = r * 0.4124 + g * 0.3576 + b * 0.1805;
+		xyz[1] = r * 0.2126 + g * 0.7152 + b * 0.0722;
+		xyz[2] = r * 0.0193 + g * 0.1192 + b * 0.9505;
+		return xyz;
+	}	
 }
--- test/java/colorworld/color/ColorConverterTest.java
+++ test/java/colorworld/color/ColorConverterTest.java
@@ -7,6 +7,13 @@ import javax.imageio.ImageIO;
 public class ColorConverterTest {
 
 	public static void main(String[] args) {
+		double[] test1 = {0,0,0};
+		double[] test2 = {100,100,100};
+		double[] test3 = {255,255,255};
+		double[] xyzResult = ColorConverter.rgbToXyz(test3);
+		for (double d : xyzResult) {
+			System.out.println(d);
+		}
 	}
 
 }
